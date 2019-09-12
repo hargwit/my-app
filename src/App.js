df623bd638e0a903f033c12b05e50977c617ff9f
Harry_W <>
2019-09-12 08:51:49 +0100

    Change page to return users to my site

--- src/App.js
+++ src/App.js
@@ -8,15 +8,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, you've probably come here from my blog post
+        </p>
+        <a
+            className="App-link"
+            href="https://hargwit.com/blog/create-a-react-app-and-deploy-to-netlify"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            How to create a React app and deploy it to Netlify
+          </a>
+        <p>
+          If you want to learn more awesome software dev stuff then you should check out my other blog posts below
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://hargwit.com/blog"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          More blog posts!
         </a>
       </header>
     </div>
